46e25bf749dc0c0adb5edb2f84c98e055b599b6a
Devashish jindal <60930823+drace28@users.noreply.github.com>
2023-10-01 21:39:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import moderngl as mgl
 import struct
 import sys
 class App:
-    
     def __init__(self, win_size=(1600, 900)):
         # opengl context
         pg.display.set_mode(win_size, flags=pg.OPENGL | pg.DOUBLEBUF)
