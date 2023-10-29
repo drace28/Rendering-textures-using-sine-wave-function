98b5e0a8a2aa854188f56dd73fe18f4fd1c85f3b
Devashish jindal <mohijindal28@gmail.com>
2023-10-29 21:10:05 +0530

    .

--- main.py
+++ main.py
@@ -5,31 +5,36 @@ import sys
 
 class App:
     def __init__(self, win_size=(1600, 900)):
-        # opengl context
-        pg.display.set_mode(win_size, flags=pg.OPENGL | pg.DOUBLEBUF)
+        # Initialize Pygame
+        pg.init()
+        
+        # Create a Pygame display
+        pg.display.set_mode(win_size, pg.OPENGL | pg.DOUBLEBUF)
+        
+        # OpenGL context
         self.ctx = mgl.create_context()
 
-        # time objects
+        # Time objects
         self.clock = pg.time.Clock()
 
-        # load shaders
-        with open("Rendering-textures-using-sine-wave-function/vertex.glsl") as f:
+        # Load shaders (Make sure file paths are correct)
+        with open("vertex.glsl") as f:
             vertex = f.read()
-        with open("Rendering-textures-using-sine-wave-function/fragment.glsl") as f:
+        with open("fragment.glsl") as f:
             fragment = f.read()
         self.program = self.ctx.program(vertex_shader=vertex, fragment_shader=fragment)
 
-        # quad screen vertices
+        # Quad screen vertices
         vertices = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, -1), (1, 1)]
 
-        # quad vbo
+        # Quad VBO
         vertex_data = struct.pack(f'{len(vertices) * len(vertices[0])}f', *sum(vertices, ()))
         self.vbo = self.ctx.buffer(vertex_data)
 
-        # quad vao
-        self.vao = self.ctx.vertex_array(self.program, [(self.vbo, '2f', 'in_position')])
+        # Quad VAO
+        self.vao = self.ctx.simple_vertex_array(self.program, self.vbo, 'in_position')
 
-        # uniforms
+        # Uniforms
         self.set_uniform('u_resolution', win_size)
 
     def render(self):
@@ -45,15 +50,15 @@ class App:
             self.check_events()
             self.update()
             self.render()
-            self.clock.tick(0)
+            self.clock.tick(60)  # Limit the frame rate to 60 FPS
             fps = self.clock.get_fps()
             pg.display.set_caption(f'{fps :.1f}')
 
     def set_uniform(self, u_name, u_value):
         try:
-            self.program[u_name] = u_value
+            self.program[u_name].value = u_value
         except KeyError:
-            pass
+            raise ValueError(f"Uniform '{u_name}' not found in shader program")
 
     def destroy(self):
         self.vbo.release()
@@ -66,8 +71,6 @@ class App:
                 self.destroy()
                 pg.quit()
                 sys.exit()
-    
-
 
 if __name__ == '__main__':
     app = App()
