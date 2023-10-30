927eba479d71a0c5dd3d7884fcaae7b3f5424317
Devashish jindal <mohijindal28@gmail.com>
2023-10-30 20:26:26 +0530

    .

--- main.py
+++ main.py
@@ -3,6 +3,49 @@ import moderngl as mgl
 import struct
 import sys
 
+
+class Quad(pg.sprite.Sprite):
+    def __init__(self, win_size):
+        super().__init__()
+        self.image = pg.Surface(win_size, pg.SRCALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.center = win_size[0] // 2, win_size[1] // 2
+
+        # Quad vertices
+        self.vertices = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, -1), (1, 1)]
+
+        # Set up Pygame drawing context
+        self.ctx = pg.display.get_surface()
+
+        # Load shaders (Make sure file paths are correct)
+        with open("vertex.glsl") as f:
+            vertex = f.read()
+        with open("fragment.glsl") as f:
+            fragment = f.read()
+        self.program = pg.gl.GLProgram(vertex, fragment)
+
+        # Uniforms
+        self.set_uniform('u_resolution', win_size)
+
+    def set_uniform(self, u_name, u_value):
+        try:
+            self.program.uniforms[u_name] = u_value
+        except KeyError:
+            raise ValueError(f"Uniform '{u_name}' not found in shader program")
+
+    def update(self, dt):
+        self.set_uniform('u_time', pg.time.get_ticks() * 0.001)
+
+    def draw(self):
+        # Clear the screen
+        self.ctx.fill((0, 0, 0))
+
+        # Draw the quad
+        pg.draw.polygon(self.image, (255, 255, 255), self.vertices)
+
+        # Render the quad using the shader program
+        self.program.use()
+        pg.gl.glDrawArrays(pg.gl.GL_TRIANGLES, 0, len(self.vertices))
 class App:
     def __init__(self, win_size=(1600, 900)):
         # Initialize Pygame
@@ -25,7 +68,7 @@ class App:
         self.program = self.ctx.program(vertex_shader=vertex, fragment_shader=fragment)
 
         # Quad screen vertices
-        vertices = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, -1), (1, 1)]
+        vertices = [(-1,-1), (1, -1), (1, 1), (-1, 1), (-1, -1), (1, 1)]
 
         # Quad VBO
         vertex_data = struct.pack(f'{len(vertices) * len(vertices[0])}f', *sum(vertices, ()))
